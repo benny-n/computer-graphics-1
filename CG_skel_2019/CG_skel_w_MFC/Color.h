553d61bb00b8f447b64ce7c09140f6277400429f
MMirbach <70845604+MMirbach@users.noreply.github.com>
2021-12-05 11:11:49 +0200

    operators for color

--- CG_skel_2019/CG_skel_w_MFC/Color.h
+++ CG_skel_2019/CG_skel_w_MFC/Color.h
@@ -1,7 +1,34 @@
 #pragma once
 
+#define CLAMP(x) (x) = ((x) > 1)? 1 : (x)
+
 struct Color {
-	float r = 1;
-	float g = 1;
-	float b = 1;
+	float r;
+	float g;
+	float b;
+
+	Color() : r(1), g(1), b(1) {}
+	Color(float r, float g, float b) : r(CLAMP(r)), g(CLAMP(g)), b(CLAMP(b)) {}
+
+	Color operator + (const Color& c) const {
+		return Color{ r + c.r, g + c.g, b + c.b };
+	}
+
+	Color operator * (const float s) const {
+		return Color{ s * r, s * g, s * b };
+	}
+
+	Color operator * (const Color& c) const {
+		return Color{ r * c.r, g * c.g, b * c.b };
+	}
+
+	friend Color operator * (const float s, const Color& c) {
+		return Color{ s * c.r, s * c.g, s * c.b };
+	}
+
+
+	friend std::ostream& operator << (std::ostream& os, const Color& c) {
+		return os << "( " << c.r << ", " << c.g << ", " << c.b << " )";
+	}
 };
+
