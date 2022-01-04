270eb4137610ae313177d95a37cf4b7f1f6813fe
MMirbach <70845604+MMirbach@users.noreply.github.com>
2022-01-04 18:28:12 +0200

    non reference

--- CG_skel_2019/CG_skel_w_MFC/Renderer.h
+++ CG_skel_2019/CG_skel_w_MFC/Renderer.h
@@ -47,7 +47,7 @@ public:
 	const mat4& getNormalTransform();
 	const mat4& getCameraTransform();
 	const mat4& getProjection();
-	const mat4& from3dTo2d();
+	const mat4 from3dTo2d();
 	void setDemoBuffer();
 	void reshape(int width, int height);
 	void colorPixel(int x, int y, const Color& color = Color{ 0.5, 0.5, 0.5 });
